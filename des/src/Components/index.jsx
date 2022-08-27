50eb8e15db62f6eae2588d5b7fc297dff472104a
Muhmmad ali <105370368+MuhammadAli8858@users.noreply.github.com>
2022-08-27 16:19:26 +0500

    des

---
+++ des/src/Components/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Component } from './style'
+
+const Navbar = () => {
+  return (
+    <Component>
+      Navbar
+    </Component>
+  )
+}
+
+export default Navbar
---
+++ des/src/Components/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+
+export const Component = styled.div`
+color: red;
+background-color: wheat;
+width: 400px;
+height: 400px;
+text-align: center;
+justify-content: center;
+align-items: center;
+
+
+` 
